fd9b3a46c3399225cdef73db5718dd7690722828
Hatem Khabir <khabirhatem38@gmail.com>
2023-03-11 14:10:29 +0100

    removed login page , useless lel asaf

--- MusicPlayer/Login and signup/FirstPage.Designer.cs
+++ MusicPlayer/Login and signup/FirstPage.Designer.cs
@@ -29,40 +29,40 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.formPanel = new System.Windows.Forms.Panel();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.siticoneDragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
             this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // formPanel
-            // 
-            this.formPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.formPanel.Location = new System.Drawing.Point(2, 11);
-            this.formPanel.Name = "formPanel";
-            this.formPanel.Size = new System.Drawing.Size(214, 472);
-            this.formPanel.TabIndex = 0;
-            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.guna2Button1);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(213, 11);
+            this.panel1.Location = new System.Drawing.Point(-1, 13);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(606, 471);
+            this.panel1.Size = new System.Drawing.Size(956, 543);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MusicPlayer.Properties.Resources.ezgif_com_gif_maker;
             this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(616, 472);
+            this.pictureBox1.Size = new System.Drawing.Size(909, 545);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -81,34 +81,62 @@
             // guna2ControlBox1
             // 
             this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.CustomizableEdges = customizableEdges5;
             this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
             this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
-            this.guna2ControlBox1.Location = new System.Drawing.Point(741, -2);
+            this.guna2ControlBox1.Location = new System.Drawing.Point(864, -2);
+            this.guna2ControlBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.guna2ControlBox1.Name = "guna2ControlBox1";
-            this.guna2ControlBox1.Size = new System.Drawing.Size(38, 18);
+            this.guna2ControlBox1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            this.guna2ControlBox1.Size = new System.Drawing.Size(44, 21);
             this.guna2ControlBox1.TabIndex = 1;
             // 
             // guna2ControlBox2
             // 
             this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.CustomizableEdges = customizableEdges3;
             this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
             this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
-            this.guna2ControlBox2.Location = new System.Drawing.Point(707, -2);
+            this.guna2ControlBox2.Location = new System.Drawing.Point(825, -2);
+            this.guna2ControlBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.guna2ControlBox2.Name = "guna2ControlBox2";
-            this.guna2ControlBox2.Size = new System.Drawing.Size(38, 18);
+            this.guna2ControlBox2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            this.guna2ControlBox2.Size = new System.Drawing.Size(44, 21);
             this.guna2ControlBox2.TabIndex = 2;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderRadius = 15;
+            this.guna2Button1.CustomizableEdges = customizableEdges1;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.LightSalmon;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.FillColor = System.Drawing.Color.Sienna;
+            this.guna2Button1.Location = new System.Drawing.Point(395, 290);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            this.guna2Button1.Size = new System.Drawing.Size(120, 40);
+            this.guna2Button1.TabIndex = 1;
+            this.guna2Button1.Text = "START";
+            this.guna2Button1.UseTransparentBackground = true;
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
             // 
             // FirstPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 484);
+            this.ClientSize = new System.Drawing.Size(910, 558);
             this.Controls.Add(this.guna2ControlBox2);
             this.Controls.Add(this.guna2ControlBox1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.formPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FirstPage";
             this.Text = "FirstPage";
             this.panel1.ResumeLayout(false);
@@ -118,13 +146,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel formPanel;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private Guna.UI2.WinForms.Guna2DragControl siticoneDragControl1;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
         private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
         private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
     }
 }
--- MusicPlayer/Login and signup/FirstPage.cs
+++ MusicPlayer/Login and signup/FirstPage.cs
@@ -1,64 +1,24 @@
 using Guna.UI2.WinForms.Enums;
-using MusicPlayer.Login_and_signup;
+using MusicPlayer.HomePage;
 using System.Windows.Forms;
 
 namespace MusicPlayer
 {
     public partial class FirstPage : Form
     {
-        bool mousedown;
-        private Point offset;
-        private Login Loginform;
         private Form activeform = null;
-
+        private Mainpage mainpage;
         public FirstPage()
         {
             InitializeComponent();
-            Loginform= new Login(this);
-            showForm(Loginform);
         }
         
-        public void showForm(Form child)
+        private void guna2Button1_Click(object sender, EventArgs e)
         {
-            if (activeform != null)
-                activeform.Hide();
-            activeform = child;
-            child.TopLevel = false;
-            child.FormBorderStyle = FormBorderStyle.None;
-            child.Dock = DockStyle.Fill;
-            formPanel.Controls.Add(child);
-            formPanel.Tag = child;
-            child.BringToFront();
-            child.Show();
+            this.Hide();
+            mainpage = new Mainpage(this);
+            mainpage.Show();
 
         }
-        private void guna2PictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void siticoneButton2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void siticoneGradientButton1_Click(object sender, EventArgs e)
-        {
-            
-            DialogResult result = MessageBox.Show("Account Created Successfully , Please use your information to login to your Account ! ", "Signup Successfull",MessageBoxButtons.OK);
-            if (result == DialogResult.OK)
-            {
-            }
-            
-             
-
-        }
-
-      
-        }
+    }
     }
